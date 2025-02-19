5029c4e4a7ad6dd181a200f75591edb5187dd71c
hmon <wtthumon@gmail.com>
2025-02-19 16:32:58 +0100

    add more data to existing user using unique path

--- crud/push.py
+++ crud/push.py
@@ -4,19 +4,24 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pyrebase
 from utils.config import firebaseConfig
 #copy original config and modify the url
-firebaseConfigHabit = {}
-for key,value in firebaseConfig.items():
-    print(key,value)
-    if key == "databaseURL":
-        firebaseConfigHabit[key] = value+"habits/"
-        print(firebaseConfigHabit[key])
-    else:
-        firebaseConfigHabit[key] = value
-
+def userDB(userPath):
+    firebaseConfigHabit = {}
+    for key,value in firebaseConfig.items():
+        print(key,value)
+        if key == "databaseURL":
+            firebaseConfigHabit[key] = value+"/habits/"+userPath
+        else:
+            firebaseConfigHabit[key] = value
+    return firebaseConfigHabit
+userPath = "-OJTQx0MIMXm4z5DeAF8"
+firebaseConfigHabit = userDB(userPath)
 firebase = pyrebase.initialize_app(firebaseConfigHabit)
 db = firebase.database() 
 
-def createNewUserID(db):
-    db.push({"habits": {1: "", 2:"2"}, "frequency":{1:"", 2:""}})
 
-createNewUserID(db)
+def createNewUserID(db):
+    db.push({"uniqueUserID": 1234})
+
+def appendToUser(db,key, value):
+    db.child(key).set(value)
+appendToUser(db, "NewKey","NewValue")
